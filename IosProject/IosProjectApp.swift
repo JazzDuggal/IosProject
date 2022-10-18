20f43db3505232b4470c67edb58d0bccc53f50b2
Jazz Duggal <s0543312@student.rockvalleycollege.edu>
2022-10-18 18:13:31 -0500

    Initial Commit

---
+++ IosProject/IosProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosProjectApp.swift
+//  IosProject
+//
+//  Created by Mac on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct IosProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
